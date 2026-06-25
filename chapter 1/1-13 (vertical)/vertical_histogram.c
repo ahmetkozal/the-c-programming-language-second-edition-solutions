c47959b0dfce13a719f5be3c081705e8946e7748
Ahmet Kozal <57260974+ahmetkozal@users.noreply.github.com>
2026-06-25 13:30:33 +0600

    Fix array size and indexing for word lengths

--- chapter 1/1-13 (vertical)/vertical_histogram.c
+++ chapter 1/1-13 (vertical)/vertical_histogram.c
@@ -3,10 +3,10 @@
 easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging.*/
 int main(){
     int c, len, max;
-    int words[20];
+    int words[21];
     len = max = 0;
     //Kullanmadan once listeyi temizliyoruz cunku ici random dolu.
-    for (int i = 0; i < 20;++i){
+    for (int i = 1; i < 21;++i){
         words[i] = 0;
     }
     while ((c = getchar()) != EOF){
@@ -17,13 +17,22 @@ int main(){
         /*araya bosluk girince kelime bitiyor, leni index olarak olarak kullanip
         len uzunlugundaki kelimeler degerini 1 arttiriyor*/
         else{
-            ++words[len];
+            //eger kelime 0 harften buyukse
+            if (len>0){
+                ++words[len];
+            }
+
             //yeni kelimeye gecmeden once sifirliyor ki yeni kelimenin uzunlugunu hesaplayalim.
             len = 0;
         }
     }
+    //EOF oldugunda son kelime 0 harften buyukse onu da eklemek icin.
+    if (len>0){
+        ++words[len];
+    }
+
     //Kac harflik kelimeden en cok var hesaplamasi. Cunku yukaridan asagi yazacagimiz icin yukseklik lazim.
-    for(int i =0;i<20;++i){
+    for(int i =1;i<21;++i){
         if (words[i]>max){
             max = words[i];
         }
@@ -31,7 +40,7 @@ int main(){
     //sutun, max'tan sifira iniyor. yukseklik
     for (int i = max; i > 0;--i){
         //satir, 20 satir olacak
-        for (int z = 0; z<20;++z){
+        for (int z = 1; z<21;++z){
             /* eger 'z' harflik kelime sayisi i'den, yani yukseklikten buyuk veya esitse.
             yani mesela 3 harflik kelimeden, su an i=4 diyelim.4 veya daha cok varsa
             * yazdiracagiz. eger 3 tane varsa yazdirmayacak, sonraki dongude i 3 oldugu zaman
@@ -46,7 +55,7 @@ int main(){
         printf("\n");
     }
     // harf sayilarini yazdirmak icin.
-    for (int i = 0; i < 20;++i){
+    for (int i = 1; i < 21;++i){
         if (i<10){
             printf("| %d |",i);
         }
