43fda90d36c93bc28546e7bc02d93daf65cb0def
ahmetkozal <ahmetkkzal@gmail.com>
2026-06-27 10:48:17 +0600

    added delete blanks function

--- chapter 1/1-18/delete_blanks.c
+++ chapter 1/1-18/delete_blanks.c
@@ -3,19 +3,39 @@
 from each line of input, and to delete entirely blank lines. */
 #define MAXLINE 1000
 
+#define IN 1
+#define OUT 0
 
-void delete_bl(char line[], char new_line[]);
-
+void delete_bl(char line[], int len);
+int getline(char line[],int maxline);
 int main(){
-	int c;
-	while((c=getchar())!=EOF){
+	int total_len,len;
+	char line[MAXLINE];
+	while ((len = getline(line,MAXLINE))>0){
+		total_len = len;
+
+		printf("|%s|\n",line);
+		delete_bl(line,total_len);
+		printf("|%s|\n",line);
 		
+		total_len = 0;
 	}
 }
 
-void delete_bl(char line[], char new_line[]){
-	int i = 0;
-	while((line[i] = new_line[i]) != '\0' && (line[i] = new_line[i]) != ' '){
-		++i;
+int getline(char s[], int limit){
+	int c,i;
+	for (i = 0; i<limit-1 && (c=getchar())!=EOF && c!='\n';++i){
+	s[i] = c;
+	}
+	s[i] = '\0';
+	return i;
+}
+
+
+void delete_bl(char line[], int len){
+	int i = len -1;
+	while(line[i]==' ' || line[i]=='\t'){
+		line[i] = '\0';
+		--i;
 	}
 }
