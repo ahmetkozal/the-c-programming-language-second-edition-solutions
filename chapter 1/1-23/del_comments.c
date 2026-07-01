77ce40f6bbb059e62ea434010a72243d31a83918
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-01 17:02:09 +0600

    deleting commit

--- chapter 1/1-23/del_comments.c
+++ chapter 1/1-23/del_comments.c
@@ -2,8 +2,20 @@
 
 #define MAXLINE 1000
 
+#define IN 1
+#define OUT 0
+
+/*Exercise 1-23. Write a program to remove all
+comments from a C program.
+Don't forget to handle quoted strings and
+character constants properly. C comments
+do not nest.*/
+
 int getinput(char s[], int limit);
 void del_comments(char from[], char to[]);
+
+int state;
+
 int main(){
     char s[MAXLINE];
     char new_s[MAXLINE];
@@ -27,10 +39,27 @@ int getinput(char s[], int limit){
     return i;
 }
 void del_comments(char from[], char to[]){
-    int i = 0;
-    while(from[i] != '\0'){
-        to[i] = from[i];
-        ++i;
-    }
-    to[i] = '\0';
+	int i_index,z_index;
+	i_index = z_index = 0;
+	state = IN;
+	while(from[i_index]!=EOF){
+		if (state == IN){
+			if (from[i_index] == 'o' && from[i_index+1] == '*'){
+				state = OUT;
+				++i_index;
+				while (state == OUT){
+					if (from[i_index] == '*' && from[i_index+1] == 'i'){
+						state = IN;
+						++i_index;
+					}
+					++i_index;
+				
+					
+				}
+			}
+			to[z_index] = from[i_index];
+			++z_index;
+			++i_index;
+	}
+	}
 }
