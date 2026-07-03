aae25f917307c5da51c2e38b627399f3c4674e53
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-03 11:16:13 +0600

    creating only symbols array is done. Now it will be easier to work on.

--- chapter 1/1-24/syntax_check.c
+++ chapter 1/1-24/syntax_check.c
@@ -4,20 +4,23 @@ for rudimentary syntax errors like unmatched parentheses
 and braces. Don't forget about quotes, both single and 
 double, escape sequences, and comments. (This program is hard
 if you do it in full generality.)*/
-/*( { [ " '*/
+/*() {} [] "" ''*/
 #define MAXLINE 1000
-#define IN 1
-#define OUT 0
+
+#define IN = 1
+#define OUT = 0
 
 int get_text(char s[], int limit);
 
-void check_for_errors(char s[]);
+void create_symbol_arr(char s[],char symbols[]);
 
 int main(){
 	char s[MAXLINE];
+	char symbols[MAXLINE];
 	get_text(s,MAXLINE);
-	printf("I:%s",s);
-	check_for_errors(s);
+	printf("I:%s\n",s);
+	create_symbol_arr(s,symbols);
+	printf("O:%s",symbols);
 	return 0;
 }
 
@@ -34,11 +37,23 @@ int get_text(char s[], int limit)
 	return i;
 }
 
-void check_for_errors(char s[])
+void create_symbol_arr(char s[], char symbols[])
 {
-	int i = 0;
+	char symbol_list[] = {"\n(){}[]\"\'\0"};
+	int i, z;
+	i = z = 0;
 	while(s[i]!='\0')
 	{
-		
+		for (int j = 0;symbol_list[j]!='\0';++j)
+		{
+			if (s[i] == symbol_list[j]){
+				symbols[z] = s[i];
+				++z;
+				
+				break;
+			}
+		}
+		++i;
 	}
+	symbols[z] = '\0';
 }
