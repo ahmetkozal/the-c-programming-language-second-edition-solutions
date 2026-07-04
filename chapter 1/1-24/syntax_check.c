769f43081a835162fb87096a696a149caf6c89e3
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-04 14:24:41 +0600

    STR CONTROL IS DONE

--- chapter 1/1-24/syntax_check.c
+++ chapter 1/1-24/syntax_check.c
@@ -49,44 +49,33 @@ int create_symbol_arr(char s[], char symbols[])
     
     char symbol_list[] = {"\n(){}[]"};
     
-    int i, z, escape_count;
-    i = z = 0;
+    int i, new_i, escape_count;
+    i = new_i = 0;
     
     while(s[i]!='\0')
     {
         //Eger " gelirse, str OUT ise, oncesi ESCAPE degil ise str=IN
-        //Eger " oncesi \->escape ise escape_count saymaya basla. escape_count % 2 != 0 ise str = IN olacak
-        if (s[i] == '"' && s[i+1] != '"' && str == OUT)
+        //Eger " oncesi \->escape ise escape_count saymaya basla. escape_count % 2 == 0 ise str = IN olacak
+        if (s[i] == '"')
         {
-            if ((i!=0 && s[i-1] == '\\') || i == 0 && s[i] == '\\')
+            escape_count = 0;
+            for (int j = i-1;j>=0 && s[j]=='\\';--j)
             {
-                for(int e = i;s[e] != '\\' || e == 0;--e)
-                {
-                    if(s[e] == '\\')
-                    {
-                        ++escape_count;
-                    }
-                }
-                if (escape_count%2!=0)
+                ++escape_count;
+            }           
+            if (escape_count%2==0)
+            {
+                if (str == OUT)
                 {
                     str = IN;
-                    ++i;
-                    
+                }
+                else
+                {
+                    str = OUT;
                 }
             }
-            else if ((i!=0 && s[i-1] != '\\') || (s[i] == 0 && s[i] != '\\'))
-            {
-                str = IN;
-                ++i;
-            }
-        }
-        else if(s[i] == '"' && str == IN)
-        {
-            str = OUT;
             ++i;
-        }
-        
-        
+        }        
         
         //sadece kontrol edecegimiz sembollerden olusan bir dizi olustur.
         if(str == OUT && cmt == OUT)
@@ -95,16 +84,16 @@ int create_symbol_arr(char s[], char symbols[])
             {
                 if (s[i] == symbol_list[j])
                 {
-                    symbols[z] = s[i];
-                    ++z;
+                    symbols[new_i] = s[i];
+                    ++new_i;
                     break;
                 }
             }
         }
         ++i;
     }
-    symbols[z] = '\0';
-    return z;
+    symbols[new_i] = '\0';
+    return new_i;
 }
 
 void check_for_errors(char s[],int arr_len)
