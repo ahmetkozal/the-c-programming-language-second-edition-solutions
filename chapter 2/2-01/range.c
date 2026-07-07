2e0bcb6c2f005e0b851b3501157120741210edad
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-08 00:47:31 +0600

    hm

--- chapter 2/2-01/range.c
+++ chapter 2/2-01/range.c
@@ -6,19 +6,19 @@ Exercise 2-1. Write a program to determine the ranges of
 !!! char, short, int, and long !!!
 variables, both signed and unsigned, by printing appropriate values from standard headers
 and by direct computation.
-??? Harder if you compute them: determine the ranges of the various floating-point types.???
+??? Harder if you com pute them: determine the ranges of the various floating-point types.???
 */
 int main(){
-    printf("%d -> %d\n",CHAR_MIN, CHAR_MAX);
-    printf("%u -> %u\n\n",CHAR_MIN, CHAR_MAX);
+    printf("SIGNED:%d -> %d\n",CHAR_MIN, CHAR_MAX);
+    printf("UNSIGNED:%d -> %d\n\n",0, UCHAR_MAX);
 
-    printf("%d -> %d\n",SHRT_MIN, SHRT_MAX);
-    printf("%u -> %u\n\n",SHRT_MIN, SHRT_MAX);
+    printf("SIGNED:%d -> %d\n",SHRT_MIN, SHRT_MAX);
+    printf("UNSIGNED:%d -> %d\n\n",0, USHRT_MAX);
 
-    printf("%d -> %d\n",INT_MIN, INT_MAX);
-    printf("%u -> %u\n",INT_MIN, INT_MAX);
+    printf("SIGNED:%d -> %d\n",INT_MIN, INT_MAX);
+    printf("UNSIGNED:%d -> %d\n",0, UINT_MAX);
 
-    printf("%ld -> %ld\n",LONG_MIN, LONG_MAX);
-    printf("%lu -> %lu\n",LONG_MIN, LONG_MAX);
+    printf("SIGNED:%ld -> %ld\n",LONG_MIN, LONG_MAX);
+    printf("UNSIGNED:%d -> %ld\n",0, ULONG_MAX);
     return 0;
 }
