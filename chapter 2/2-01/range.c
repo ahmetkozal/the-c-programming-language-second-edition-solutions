19580e6d89cd9f2fed7223f14295ff2ec26f1999
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-08 00:10:03 +0600

    Trying to figure out what exercise actually wants.

---
+++ chapter 2/2-01/range.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+/*Exercise 2-1. Write a program to determine the ranges of
+char, short, int, and long
+variables, both signed and unsigned, by printing appropriate values from standard headers
+and by direct computation. Harder if you compute them: determine the ranges of the various
+floating-point types. */
+int main(){
+    printf("%d -> %d\n",CHAR_MIN, CHAR_MAX);
+    return 0;
+}
