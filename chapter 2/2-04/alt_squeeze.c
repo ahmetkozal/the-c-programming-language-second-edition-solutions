09d2917696b1c6cb7c1d5de4c379b57bd2a21075
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-09 08:50:59 +0600

    Added input

--- chapter 2/2-04/alt_squeeze.c
+++ chapter 2/2-04/alt_squeeze.c
@@ -2,13 +2,20 @@
 /*Exercise 2-4. Write an alternative version of squeeze(s1,s2)
 that deletes each character in s1 that matches any character
 in the string s2.*/
+#define MAXLINE 1000
 void squeeze(char s1[],char s2[]);
+int get_text(char s[],int limit);
 int main(){
-    char s1[] = "abcabc";
-    char s2[] = "ac";
-    printf("INITIAL:\t%s\n",s1);
+    int limit = MAXLINE;
+    char s1[MAXLINE];
+    char s2[MAXLINE];
+    printf("ENTER THE FIRST TEXT: ");
+    get_text(s1,limit);
+    printf("ENTER THE SECOND TEXT: ");
+    get_text(s2,limit);
+    printf("INITIAL:\n%s\n",s1);
     squeeze(s1,s2);
-    printf("SQUEEZED:\t%s\n",s1);
+    printf("SQUEEZED:\n%s\n",s1);
     return 0;
 }
 void squeeze(char s1[], char s2[]){
@@ -30,4 +37,12 @@ void squeeze(char s1[], char s2[]){
         ++read_i;
     }
     s1[write_i] = '\0';
+}
+int get_text(char s[], int limit){
+    int c, i;
+    for (i = 0; (c=getchar())!=EOF && i < limit-1 && c!='\n'; ++i){
+        s[i] = c;
+    }
+    s[i] = '\0';
+    return i;
 }
