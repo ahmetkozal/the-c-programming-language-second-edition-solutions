7aecc6ed1ee4c54fc24113afea11fe2ce1b0942f
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-09 13:43:47 +0600

    starting to bits

---
+++ chapter 2/2-06/set_bits.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*Exercise 2-6. Write a function setbits(x,p,n,y) that returns x 
+with the n bits that begin at position p set to the rightmost 
+n bits of y, leaving the other bits unchanged. */
+int set_bits(int x, int p, int n, int y);
+int main(){
+    
+    return 0;
+}
