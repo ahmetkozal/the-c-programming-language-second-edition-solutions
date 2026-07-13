39dc90c0bb29619045ee7b110dcc393acd0f416b
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-13 15:47:35 +0600

    Solved 3.1

--- chapter 3/3-01/binsearch.c
+++ chapter 3/3-01/binsearch.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
+#include <time.h>
 /*Exercise 3-1. Our binary search makes two tests inside the loop, 
 when one would suffice (at the price of more tests outside.) 
 Write a version with only one test inside the loop and measure 
 the difference in run-time. */
-#define MAXLINE 1000
+#define MAXLINE 10000
 int binsearch(int x, int v[], int n);
 int new_binsearch(int x, int v[], int n);
 int new_binsearch(int x, int v[], int n);
 int main(){
-    int x, n;
-    x = 2;
-    n = 9;
-    int v[MAXLINE] = {1,2,3,4,5,6,7,8,9};
-    printf("OLD: %i\n",binsearch(x,v,n));
-    
-    printf("NEW: %i\n",new_binsearch(x,v,n));
+    int x = 5000;
+    int v[MAXLINE];
+	int n = MAXLINE;
+	for (int i = 0; i < n; i++) {
+		v[i] = i + 1;  // 1'den 10000'e kadar
+	}
+	
+	clock_t start = clock();
+	for (int i = 0; i<=100000000;++i){
+		binsearch(x,v,n);
+	}
+	clock_t end = clock();
+	double elapsed = (double)(end - start) / CLOCKS_PER_SEC;
+	printf("OLD Zaman: %f saniye\n", elapsed);
+	
+	clock_t start_new = clock();
+	for (int i = 0; i<=100000000;++i){
+		new_binsearch(x,v,n);
+	}
+    clock_t end_new = clock();
+	double new_elapsed = (double)(end_new - start_new) / CLOCKS_PER_SEC;
+    printf("NEW Zaman: %f saniye\n", new_elapsed);
     return 0;
 }
 int new_binsearch(int x, int v[], int n){
@@ -22,14 +38,17 @@ int new_binsearch(int x, int v[], int n){
     low = 0;
     high = n-1;
     mid = (low + high) /2 ;
-    while (low <= high && x!=v[mid]){
-        if(x < v[mid])
-            high = mid - 1;
-        else
-            low = mid + 1;
-        mid = (low + high) /2 ;
-    }
-    return (v[mid] == x) ? mid : -1;
+	if (v[mid] == x){
+		return mid;
+	}
+	else{
+		while (low <= high){
+			if(x < v[mid]) high = mid - 1;
+			else low = mid + 1;
+			mid = (low + high) /2 ;
+		}
+		return (v[mid] == x) ? mid : -1;
+	}
 }
 
 int binsearch(int x, int v[], int n){
@@ -39,7 +58,7 @@ int binsearch(int x, int v[], int n){
     while (low <= high) {
         mid = (low + high)/2;
         if (x < v[mid])
-            high = mid + 1;
+            high = mid - 1;
         else if (x > v[mid])
             low = mid + 1;
         else
