1b31df43a428f26feec298ec2d52bfb280a078d4
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-14 09:30:01 +0600

    Added other escapes

--- chapter 3/3-02/escape.c
+++ chapter 3/3-02/escape.c
@@ -7,7 +7,7 @@ well, converting escape sequences into the real characters.*/
 #define MAXLINE 1000
 void escape(char s[], char t[]);
 int main(){
-    char s[] = "a\ta";
+    char s[] = "a\ta\tb\na";
     char t[MAXLINE];
     
     printf("INITAL: %s\n",s);
@@ -22,20 +22,70 @@ void escape(char s[], char t[]){
     while (s[i]!='\0'){
         c = s[i];
         switch(c){
-            case '\t':
+            case '\a':
                 t[j++] = '\\';
-                t[j++] = 't';
+                t[j++] = 'a';
                 ++i;
                 break;
-            case '\n':
+            case '\b':
+                t[j++] = '\\';
+                t[j++] = 'b';
+                ++i;
+                break;
+			case '\e':
+                t[j++] = '\\';
+                t[j++] = 'e';
+                ++i;
+                break;
+			case '\f':
+                t[j++] = '\\';
+                t[j++] = 'f';
+                ++i;
+                break;
+			case '\n':
                 t[j++] = '\\';
                 t[j++] = 'n';
                 ++i;
                 break;
+			case '\r':
+                t[j++] = '\\';
+                t[j++] = 'r';
+                ++i;
+                break;
+			case '\t':
+                t[j++] = '\\';
+                t[j++] = 't';
+                ++i;
+                break;
+			case '\v':
+                t[j++] = '\\';
+                t[j++] = 'v';
+                ++i;
+                break;
+			case '\\':
+                t[j++] = '\\';
+                t[j++] = '\\';
+                ++i;
+                break;
+			case '\'':
+                t[j++] = '\\';
+                t[j++] = '\'';
+                ++i;
+                break;
+			case '\"':
+                t[j++] = '\\';
+                t[j++] = '\"';
+                ++i;
+                break;
+			case '\?':
+                t[j++] = '\\';
+                t[j++] = '?';
+                ++i;
+                break;
+			
             default:
                 t[j++] = s[i++];
         }
-
     }
     t[j] = '\0';
 }
