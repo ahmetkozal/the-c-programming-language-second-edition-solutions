c1a5ec0fa43410ddc39670b9ce1664173b254e0e
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-14 10:03:01 +0600

    added reverse_escaoe function.

--- chapter 3/3-02/escape.c
+++ chapter 3/3-02/escape.c
@@ -6,13 +6,17 @@ Use a switch. Write a function for the other direction as
 well, converting escape sequences into the real characters.*/
 #define MAXLINE 1000
 void escape(char s[], char t[]);
+void reverse_escape(char s[], char t[]);
 int main(){
     char s[] = "a\ta\tb\na";
     char t[MAXLINE];
-    
+    char r[MAXLINE];
     printf("INITAL: %s\n",s);
     escape(s,t);
-    printf("FINAL: %s\n",t);
+    printf("ESCAPED: %s\n",t);
+    reverse_escape(t,r);
+    printf("REVERSED: %s\n",r);
+    
     
     return 0;
 }
@@ -32,59 +36,124 @@ void escape(char s[], char t[]){
                 t[j++] = 'b';
                 ++i;
                 break;
-			case '\e':
+            case '\e':
                 t[j++] = '\\';
                 t[j++] = 'e';
                 ++i;
                 break;
-			case '\f':
+            case '\f':
                 t[j++] = '\\';
                 t[j++] = 'f';
                 ++i;
                 break;
-			case '\n':
+            case '\n':
                 t[j++] = '\\';
                 t[j++] = 'n';
                 ++i;
                 break;
-			case '\r':
+            case '\r':
                 t[j++] = '\\';
                 t[j++] = 'r';
                 ++i;
                 break;
-			case '\t':
+            case '\t':
                 t[j++] = '\\';
                 t[j++] = 't';
                 ++i;
                 break;
-			case '\v':
+            case '\v':
                 t[j++] = '\\';
                 t[j++] = 'v';
                 ++i;
                 break;
-			case '\\':
+            case '\\':
                 t[j++] = '\\';
                 t[j++] = '\\';
                 ++i;
                 break;
-			case '\'':
+            case '\'':
                 t[j++] = '\\';
                 t[j++] = '\'';
                 ++i;
                 break;
-			case '\"':
+            case '\"':
                 t[j++] = '\\';
                 t[j++] = '\"';
                 ++i;
                 break;
-			case '\?':
+            case '\?':
                 t[j++] = '\\';
                 t[j++] = '?';
                 ++i;
                 break;
-			
+            
             default:
                 t[j++] = s[i++];
+        }
+    }
+    t[j] = '\0';
+}
+void reverse_escape(char s[], char t[]){
+    int i,j,c,next_c;
+    i = j = 0;
+    while(s[i]!='\0'){
+        c = s[i];
+        next_c = s[i+1];
+        if (c == '\\'){
+            switch (next_c){
+                case 'a':
+                    t[j++] = '\a';
+                    i+=2;
+                    break;
+                case 'b':
+                    t[j++] = '\b';
+                    i+=2;
+                    break;
+                case 'e':
+                    t[j++] = '\e';
+                    i+=2;
+                    break;
+                case 'f':
+                    t[j++] = '\f';
+                    i+=2;
+                    break;
+                case 'n':
+                    t[j++] = '\n';
+                    i+=2;
+                    break;
+                case 'r':
+                    t[j++] = '\r';
+                    i+=2;
+                    break;
+                case 't':
+                    t[j++] = '\t';
+                    i+=2;
+                    break;
+                case 'v':
+                    t[j++] = '\v';
+                    i+=2;
+                    break;
+                case '\\':
+                    t[j++] = '\\';
+                    i+=2;
+                    break;
+                case '\'':
+                    t[j++] = '\'';
+                    i+=2;
+                    break;
+                case '"':
+                    t[j++] = '\"';
+                    i+=2;
+                    break;
+                case '?':
+                    t[j++] = '\?';
+                    i+=2;
+                    break;
+                default:
+                    t[j++] = s[i++];
+            }
+        }else{
+            t[j++] = s[i++];
         }
     }
     t[j] = '\0';
