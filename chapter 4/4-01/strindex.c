bbd5ef1904cbcb5eee16ca478952c3bbc15bbdbd
ahmetkozal <ahmetkkzal@gmail.com>
2026-07-16 09:12:57 +0600

    solved 4.1

--- chapter 4/4-01/strindex.c
+++ chapter 4/4-01/strindex.c
@@ -4,18 +4,24 @@
 the position of the rightmost occurrence of t in s, or -1 if
 there is none. */
 #define MAXLINE 1000
-int strindex(char s[], int t);
+int strindex(char s[], char t[]);
 int main() {
-    char s[] = "This is the test input";
-    printf("%i\n",strindex(s,'u'));
+    char s[] = "This is the test st yeah";
+    char t[] = "est";
+    printf("%i\n",strindex(s,t));
     return 0;
 }
 
-int strindex(char s[], int t) {
-    int i;
-    for (i = strlen(s) - 1; i >= 0; --i) {
-        if (s[i] == t)
-            return i;
+int strindex(char s[], char t[]) {
+    int i,j,k,len_s,len_t;
+    len_s = strlen(s);
+    len_t = strlen(t);
+    
+    for (i = len_s-1;i>=len_t-1;--i){
+        for (j=i, k=len_t-1;k>=0 && s[j]==t[k] ;--k,--j)
+            ;
+        if (k == -1)
+            return i - len_t +1;
     }
     return -1;
 }
